2e7109d3ced451965721d930379bacf4b12c6e4d
franco1882 <103392506+franco1882@users.noreply.github.com>
2022-04-10 21:23:18 -0300

    Update index.js

--- index.js
+++ index.js
@@ -18,6 +18,26 @@ client.on("ready", () => {
    if(message.content === "Línea H")
    message.channel.send("La Línea H Está funcionando con normalidad.");
    }
+           
+   if(message.content === "Línea A")
+   message.channel.send("La Línea A Está funcionando con normalidad.");
+
+   if(message.content === "Línea B")
+   message.channel.send("La Línea B Está funcionando con normalidad.");
+   }
+   
+   if(message.content === "Línea C")
+   message.channel.send("La Línea H Está funcionando con normalidad.");
+   }
+   
+   if(message.content === "Línea D")
+   message.channel.send("La Línea D Está funcionando con normalidad.");
+   }
+   
+   if(message.content === "Línea E")
+   message.channel.send("La Línea E Está funcionando con normalidad.");
+   }
+        
    
 
 
